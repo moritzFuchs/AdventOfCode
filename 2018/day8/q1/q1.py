6f2d0841447a170f74da9c5ad0c13512d3bd02d2
Moritz Fuchs <moritz.fuchs@gmail.com>
2018-12-08 10:38:15 +0100

    Add day8 solutions

---
+++ 2018/day8/q1/q1.py
@@ -0,0 +1,26 @@
+import string
+from collections import defaultdict
+import heapq
+
+input = "input.in"
+file = open(input, "r")
+
+def doIt(inp):
+	total = 0
+	nc = inp[0]
+	mc = inp[1]
+	
+	inp = inp[2:]
+	for _ in range(nc):
+		t, inp = doIt(inp)
+		total += t
+	
+	idx = 0
+	for _ in range(mc):
+		total += inp[idx]
+		idx += 1
+	
+	return (total, inp[idx:])
+
+for line in file:
+	print(doIt(list(map(int, line.split())))[0])
---
+++ 2018/day8/q2/q2.py
@@ -0,0 +1,34 @@
+import string
+from collections import defaultdict
+import heapq
+
+input = "sample.in"
+file = open(input, "r")
+
+def doIt(inp):
+	total = 0
+	nc = inp[0]
+	mc = inp[1]
+	
+	inp = inp[2:]
+	if nc == 0:
+		idx = 0
+		for _ in range(mc):
+			total += inp[idx]
+			idx += 1
+		return total, inp[idx:]
+	else:
+		v = defaultdict(int)
+		for i in range(nc):
+			t, inp = doIt(inp)
+			v[i+1] = t
+		
+		idx = 0
+		for _ in range(mc):
+			total += v[inp[idx]]
+			idx += 1
+	
+	return (total, inp[idx:])
+
+for line in file:
+	print(doIt(list(map(int, line.split())))[0])
