94c03aaab199a2823a75e9b5d4f7fea662b387fa
Moritz Fuchs <moritz.fuchs@gmail.com>
2018-12-14 18:52:07 +0100

    Add day 14 solutions

---
+++ 2018/day14/q1/q1.py
@@ -0,0 +1,14 @@
+input = 320851
+
+state = [3,7]
+p1,p2 = 0,1
+
+while len(state) < input + 10:
+	for z in str(state[p1] + state[p2]):
+		state.append(int(z))
+	p1 += 1+state[p1]
+	p1 %= len(state)
+	p2 += 1+state[p2]
+	p2 %= len(state)
+
+print("".join(str(x) for x in state[input:input+10]))
